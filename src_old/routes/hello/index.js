973aadba3196373983619cfead5e97b2aac58d1d
Ennis Machta <esmachta@gmail.com>
2025-05-12 00:50:57 -0700

    chore: remove old files

--- src_old/routes/hello/index.js
+++
@@ -1,9 +0,0 @@
-// filepath: /Users/ennismachta/Grubber-Server/hello.js
-const express = require('express');
-const router = express.Router();
-
-router.post('/hello', (req, res) => {
-  res.status(200).json({ message: 'Hello, World ENNINS!' });
-});
-
-module.exports = router;
