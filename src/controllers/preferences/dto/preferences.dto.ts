acacb8e345cdd868f0141ef63586908d4e25c8b2
omar jandali <omarjandali93@gmail.com>
2025-06-05 17:57:31 -0700

    chore: fixed the field descriptions fro preferences

--- src/controllers/preferences/dto/preferences.dto.ts
+++ src/controllers/preferences/dto/preferences.dto.ts
@@ -6,21 +6,9 @@ export class CreatePreferencesDto {
   @IsString()
   user_id!: string;
 
-  @ApiProperty({ description: 'User theme preference' })
-  @IsString()
-  theme!: string;
-
-  @ApiProperty({ description: 'User language preference' })
-  @IsString()
-  language!: string;
-
-  @ApiProperty({ description: 'Whether notifications are enabled' })
-  @IsBoolean()
-  notifications_enabled!: boolean;
-
   @IsArray()
   @IsString({ each: true })
-  @IsOptional()
+  @ApiProperty({ description: 'User theme preference' })
   favorite_cuisines?: string[] = [];
 
   @IsArray()
@@ -32,6 +20,82 @@ export class CreatePreferencesDto {
   @IsString({ each: true })
   @IsOptional()
   cuisines_willing_to_try?: string[] = [];
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  diet_type?: string[] = [];
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  food_allergies?: string[] = [];
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  health_preferences?: string[] = [];
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  dietary_restrictions?: string[] = [];
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  preferred_meal_types?: string[] = [];
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  dining_time_preferences?: string[] = [];
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  meal_occasion_preferences?: string[] = [];
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  preferred_flavors?: string[] = [];
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  disliked_flavors?: string[] = [];
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  preferred_atmosphere?: string[] = [];
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  dining_companions?: string[] = [];
+
+  @IsString()
+  @IsOptional()
+  dining_frequency?: string;
+
+  @IsString()
+  @IsOptional()
+  primary_location?: string;
+
+  @IsString()
+  @IsOptional()
+  travel_distance?: string;
+
+  @IsString()
+  @IsOptional()
+  willingness_to_explore?: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  rating_criteria?: string[] = [];
 }
 
 export class UpdatePreferencesDto {
